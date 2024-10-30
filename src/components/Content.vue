054fcc519286b7c3d06a80c6b28904f3e89a78ff
Leilanigreer <leilani@lpcgolf.com>
2024-10-30 16:48:54 -0700

    movies index

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,8 +1,33 @@
-<script></script>
+<script>
+import axios from "axios";
+import MoviesIndex from "./MoviesIndex.vue"
+
+export default {
+  components: {
+    MoviesIndex,
+  },
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    this.handleIndexMovies();
+  },
+  methods: {
+    handleIndexMovies: function () {
+      axios.get("http://localhost:5000/movies.json").then((response) => {
+        console.log("movies index", response);
+        this.movies = response.data;
+      });
+    },
+  },
+};
+</script>
 
 <template>
   <main>
-    <h1>Welcome to Vue!</h1>
+    <MoviesIndex v-bind:movies="movies" />
   </main>
 </template>
 
---
+++ src/components/MoviesIndex.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: {
+    movies: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All MOVIES</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>{{movie.name}}</h2>
+      <p> Year: {{ movie.year }}</p>
+      <p> Genre: {{ movie.genre }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
